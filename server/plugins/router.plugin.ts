0408ff92aee92831c12e8bfee173ec597cf8ffef
AshirbadGudu <ashirbadapanigrahi@gmail.com>
2024-03-10 20:00:06 +0530

    readdir promises used

--- server/plugins/router.plugin.ts
+++ server/plugins/router.plugin.ts
@@ -1,28 +1,27 @@
 import { Express } from "express";
-import { readdir } from "fs";
+import { readdir } from "fs/promises";
 import path from "path";
 import { configs } from "../configs";
 import { errorMiddleware } from "../middlewares";
 
 export const RouterPlugin = {
-  plug(app: Express) {
-    readdir(path.join(__dirname, "../routes"), (err, files) => {
-      if (!files) return;
-      files.forEach(async (filename, index) => {
-        const route = filename.split(".")[0];
-        const routePath = path.join(__dirname, `../routes/${filename}`);
-        const { router } = await import(routePath);
+  async plug(app: Express) {
+    const files = await readdir(path.join(__dirname, "../routes"));
+    if (!files) return;
+    files.forEach(async (filename, index) => {
+      const route = filename.split(".")[0];
+      const routePath = path.join(__dirname, `../routes/${filename}`);
+      const { router } = await import(routePath);
 
-        app.use(`/${configs.API_VERSION}/${route}`, router);
+      app.use(`/${configs.API_VERSION}/${route}`, router);
 
-        console.log(
-          `http://localhost:${configs.PORT}/${configs.API_VERSION}/${route}\n`
-        );
+      console.log(
+        `http://localhost:${configs.PORT}/${configs.API_VERSION}/${route}\n`
+      );
 
-        if (files?.length - 1 !== index) return;
+      if (files?.length - 1 !== index) return;
 
-        app.use(errorMiddleware);
-      });
+      app.use(errorMiddleware);
     });
   },
 };
