850d928aa571543f9c93b2804445cbe5f8e865b1
AshirbadGudu <ashirbadapanigrahi@gmail.com>
2024-03-12 18:34:32 +0530

    count added in route

--- server/plugins/router.plugin.ts
+++ server/plugins/router.plugin.ts
@@ -1,21 +1,19 @@
 import { Express } from "express";
 import { readdir } from "fs/promises";
 import path from "path";
-import { configs } from "../configs";
 import { errorMiddleware } from "../middlewares";
+import { getRouteInfo } from "../utils";
 
 export const RouterPlugin = {
   async plug(app: Express) {
     const files = await readdir(path.join(__dirname, "../routes"));
     if (!files) return;
     files.forEach(async (filename, index) => {
-      const route = filename.split(".")[0];
-      const routePath = path.join(__dirname, `../routes/${filename}`);
-      const apiPath = `${configs.API_VERSION}/${route} 🔥\n`;
+      const { apiPath, apiURL, routePath } = getRouteInfo(filename);
 
       const { router } = await import(routePath);
       app.use(`/${apiPath}`, router);
-      console.log(`🔥 http://localhost:${configs.PORT}/${apiPath}`);
+      console.log(`${index + 1}. ${apiURL} 🔥\n`);
 
       if (files?.length - 1 !== index) return;
       app.use(errorMiddleware);
