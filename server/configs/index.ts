6ce3cbf997589098d68c37da34d219f334fe2bbe
AshirbadGudu <ashirbadapanigrahi@gmail.com>
2024-03-12 19:36:20 +0530

    mail configs added

--- server/configs/index.ts
+++ server/configs/index.ts
@@ -6,6 +6,11 @@ const configs = {
   PORT: process.env.PORT,
   JWT_SECRET: `${process.env.JWT_SECRET}`,
   API_VERSION: `api/v1`,
+  // write all the mail configurations here
+  MAIL_FROM: process.env.MAIL_FROM,
+  MAIL_USER: process.env.MAIL_USER,
+  MAIL_PASSWORD: process.env.MAIL_PASSWORD,
+  MAIL_SERVICE: process.env.MAIL_SERVICE,
 };
 
 export { configs };
